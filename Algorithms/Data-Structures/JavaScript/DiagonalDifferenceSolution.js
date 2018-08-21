b8bb71b501242395ae6874ff6f300fb64cb99cf3
Nick Sladic <nick.sladic@g.uky.edu>
2018-08-21 15:47:33 -0400

    added PlusMinus Challenge

--- Algorithms/Data-Structures/JavaScript/DiagonalDifferenceSolution.js
+++ Algorithms/Data-Structures/JavaScript/DiagonalDifferenceSolution.js
@@ -3,7 +3,7 @@ function diagonalDifference(arr) {
   var sumf = 0, sumr = 0, j = arr.length-1;
   for(var i = 0; i < arr.length; i++){
     sumf += arr[i][i];
-    sumr += arr[j][j];
+    sumr += arr[i][j];
     j--;
   }
   return Math.abs(sumf - sumr);
