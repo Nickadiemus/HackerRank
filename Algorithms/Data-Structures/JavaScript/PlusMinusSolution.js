b8138a54d56291adbccfe98c70d803e7547cca14
Nick Sladic <nick.sladic@g.uky.edu>
2018-08-22 14:32:35 -0400

    added plusMinus solution; Fix decimal output to 6;

--- Algorithms/Data-Structures/JavaScript/PlusMinusSolution.js
+++ Algorithms/Data-Structures/JavaScript/PlusMinusSolution.js
@@ -1,3 +1,18 @@
-function readLine() {
-    return inputString[currentLine++];
+// Complete the plusMinus function below.
+function plusMinus(arr) {
+  var p = 0, n = 0, z = 0;
+  const size = arr.length;
+  let arrcp = arr.sort((a,b) => {
+    return a - b;
+  })
+  arrcp.forEach((i) => {
+    if(i > 0) p++
+    else if(i <  0) n++
+    else z++
+  })
+  // FIXME: Make sure to output up to 6 decimals
+  console.log(p/size);
+  console.log(n/size);
+  console.log(z/size);
 }
+plusMinus([-4,3,-9,0,4,1])
