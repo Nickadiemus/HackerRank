145e2a0860e6943ce3834cd0ec0f8058c211c4dc
Nick Sladic <nick.sladic@g.uky.edu>
2019-02-01 10:50:36 -0500

    new challenges complete

--- Problem Solving/Data-Structures/Python/2DArray.py
+++ Problem Solving/Data-Structures/Python/2DArray.py
@@ -1,30 +1,31 @@
-# def research():
-#     wow ="Wow"
-#     for i in range(100000):
-#         wow += " " + wow
-#         print(wow)
-#
-# research()
-#
-def computeHourGlass(position, arr):
-    hgp = lambda p: [[p - 1, p - 1], [p, p - 1], [p + 1, p - 1], [ p, p ], [p - 1, p + 1],[p, p + 1],[p + 1, p + 1]]
-    hg = hgp(position)
+import sys
+
+def computeHourGlass(x,y, array):
+    hgp = lambda x,y: [
+        [x-1,y-1],[x-1,y],[x-1,y+1],
+                    [x,y],
+        [x+1,y-1],[x+1,y],[x+1,y+1]
+    ]
+    hg = hgp(x,y)
     sum = 0
     for i in range(len(hg)):
-        sum = sum + arr[hg[i][0],hg[i][1]]
+        sum += array[hg[i][0]][hg[i][1]]
     return sum
 
 
 # Complete the hourglassSum function below.
 def hourglassSum(arr):
-    max = 0
-    for x in range(1,2):
-        for y in range(1,2):
-            cur = computeHourGlass(arr[x][y],arr)
+    max = -sys.maxsize-1
+    print(max)
+    for x in range(1,5):
+        for y in range(1,5):
+            # print(arr[x][y], sep=",")
+            cur = computeHourGlass(x,y,arr)
             if(cur > max):
                 max = cur
                 print(max)
 
+    return max
 arr = \
     [
     [1,1,1,0,0,0],
@@ -33,6 +34,16 @@ arr = \
     [0,0,2,4,4,0],
     [0,0,0,2,0,0],
     [0,0,1,2,4,0]
+    ]
+
+arr1 = \
+    [
+[-1,-1,0,-9,-2,-2],
+[-2,-1,-6,-8,-2,-5],
+[-1,-1,-1,-2,-3,-4],
+[-1,-9,-2,-4,-4,-5],
+[-7,-3,-3,-2,-9,-9],
+[-1,-3,-1,-2,-4,-5],
     ]
 
 hourglassSum(arr)
